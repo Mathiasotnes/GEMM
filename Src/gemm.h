54f415fa2a52dfd18ab3a9383ddb7d020591ac48
Mathias Otnes <Mathias.otnes@gmail.com>
2024-12-03 16:49:04 -0800

    CUDA wrapper for CPU GMEMM

--- Src/gemm.h
+++ Src/gemm.h
@@ -9,6 +9,7 @@
 #define GEMM_H
 
 void gemm_cpu(float* A, float* B, float* C, int N);
+void gemm_cpu_wrapper(float* A_d, float* B_d, float* C_d, int N);
 void gemm_naive(float* A, float* B, float* C, int N);
 void gemm_opt(float* A, float* B, float* C, int N);
 
